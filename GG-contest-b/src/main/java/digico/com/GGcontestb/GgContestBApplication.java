68926c4e157d7f4d876d52e8e8d3aec6889208e7
TayLee <89365465+taehyuklee@users.noreply.github.com>
2023-03-30 10:18:07 +0900

    ggContest-b

---
+++ GG-contest-b/src/main/java/digico/com/GGcontestb/GgContestBApplication.java
@@ -0,0 +1,13 @@
+package digico.com.GGcontestb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgContestBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgContestBApplication.class, args);
+	}
+
+}
---
+++ GG-contest-b/src/test/java/digico/com/GGcontestb/GgContestBApplicationTests.java
@@ -0,0 +1,13 @@
+package digico.com.GGcontestb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgContestBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
